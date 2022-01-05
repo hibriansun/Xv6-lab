39df26fbbd1de92d29c8971439d637ec88bc53cf
briansun <imbriansun@gmail.com>
2022-01-05 23:27:06 +0800

    Using threads and locks to speed up a parallel hash table.(细化锁的粒度可以加速)

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -13,9 +13,12 @@ struct entry {
   int value;
   struct entry *next;
 };
-struct entry *table[NBUCKET];
+struct entry *table[NBUCKET];   // 共享资源 面临race condition -- sharing object, face to race condition
 int keys[NKEYS];
 int nthread = 1;
+
+// each bucket per lock
+pthread_mutex_t lock_hash_table_bucket[NBUCKET];
 
 double
 now()
@@ -31,21 +34,32 @@ insert(int key, int value, struct entry **p, struct entry *n)
   struct entry *e = malloc(sizeof(struct entry));
   e->key = key;
   e->value = value;
-  e->next = n;
+  e->next = n;  // 新的entry放入table[i]中，旧的entry放到新的entry后(next)
   *p = e;
 }
+
+// 链哈希：
+// 有NBUCKET个桶，key通过哈希函数后得到该放的桶，每个桶只有一个entry链表
+// 可能有多个key经过哈希函数后会归为同一桶，那么我们就在该桶向后延展成链
+// 如果该桶存entry链上某个key已经存过则更新entry的value，如果桶们未存过值为key的entry则新建entry放入桶中
 
 static 
 void put(int key, int value)
 {
+  // hash function
   int i = key % NBUCKET;
 
   // is the key already present?
+  // 这部分需要加锁 hashtable属于共享资源 一旦处于insert正在更新桶的状态那我们就不应该访问
+  pthread_mutex_lock(&lock_hash_table_bucket[i]);
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
   }
+  pthread_mutex_unlock(&lock_hash_table_bucket[i]);
+  
+  pthread_mutex_lock(&lock_hash_table_bucket[i]);
   if(e){
     // update the existing key.
     e->value = value;
@@ -53,6 +67,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&lock_hash_table_bucket[i]);
 }
 
 static struct entry*
@@ -113,6 +128,10 @@ main(int argc, char *argv[])
   assert(NKEYS % nthread == 0);
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
+  }
+
+  for (int i = 0; i < NBUCKET; i++) {
+    pthread_mutex_init(&lock_hash_table_bucket[i], NULL);
   }
 
   //
