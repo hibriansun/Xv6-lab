e6192238ac51d039f13ff5368cefd23dd1b86836
briansun <imbriansun@gmail.com>
2022-01-05 20:40:08 +0800

    Bug-version: uthread stack setting wrong

--- user/uthread.c
+++ user/uthread.c
@@ -10,16 +10,43 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct user_context {
+  // Register needed when the process running
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
-  char       stack[STACK_SIZE]; /* the thread's stack */
-  int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  // thread_X函数执行在这个用户栈上，如果栈指针设置错误执行过程中可能对state值改写导致bug(正是第一次无法通过的原因)
+  char                  stack[STACK_SIZE]; /* the thread's stack */
+  int                   state;             /* FREE, RUNNING, RUNNABLE */
+  struct user_context   context;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(uint64, uint64);    // uthread_switch.S
               
+void printDebugInfo(){
+  printf("th_0: state: %d\n", all_thread->state);
+  printf("th_1: state: %d\n", (all_thread+1)->state);
+  printf("th_2: state: %d\n", (all_thread+2)->state);
+  printf("th_3: state: %d\n", (all_thread+3)->state);
+}
+
 void 
 thread_init(void)
 {
@@ -32,11 +59,12 @@ thread_init(void)
   current_thread->state = RUNNING;
 }
 
+// main()、thread_yield触发调度
 void 
 thread_schedule(void)
 {
   struct thread *t, *next_thread;
-
+  // printDebugInfo();
   /* Find another runnable thread. */
   next_thread = 0;
   t = current_thread + 1;
@@ -61,8 +89,9 @@ thread_schedule(void)
     current_thread = next_thread;
     /* YOUR CODE HERE
      * Invoke thread_switch to switch from t to next_thread:
-     * thread_switch(??, ??);
+     * thread_switch(t, next_thread);
      */
+    thread_switch((uint64)&t->context, (uint64)&next_thread->context);
   } else
     next_thread = 0;
 }
@@ -76,7 +105,12 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+
+  memset(&t->context, 0, sizeof(t->context));
+  // set ra the function
+  t->context.ra = (uint64)func;
+  // set up the stack 
+  t->context.sp = (uint64)t->stack;
 }
 
 void 
