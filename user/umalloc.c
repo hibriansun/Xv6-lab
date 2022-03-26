36ab3dd312687d3f9ea068966fc99da2bd564acd
briansun <imbriansun@gmail.com>
2022-03-26 21:39:09 +0800

    malloc implementation

--- user/umalloc.c
+++ user/umalloc.c
@@ -6,20 +6,31 @@
 // Memory allocator by Kernighan and Ritchie,
 // The C programming Language, 2nd ed.  Section 8.7.
 
-typedef long Align;
+// 当有申请请求时，malloc 将扫描空闲块链表，直到找到一个足够大的块为止。该算法称
+// 为“首次适应”（first fit）；与之相对的算法是“最佳适应”（best fit），它寻找满足条件的最小
+// 块。如果该块恰好与请求的大小相符合，则将它从链表中移走并返回给用户。如果该块太大，
+// 则将它分成两部分：大小合适的块返回给用户，剩下的部分留在空闲块链表中。如果找不到
+// 一个足够大的块，则向操作系统申请一个大块并加入到空闲块链表中
 
-union header {
+// Block organization: https://s2.loli.net/2022/03/26/EnrZyUxTGpdm1gj.png
+// Header: https://s2.loli.net/2022/03/26/ZWnwgh5ltoafRcq.png
+
+// 空闲块包含一个指向链表中下一个块的指针、一个块大小的记录和
+// 一个指向空闲空间本身的指针(free data start address)
+typedef long Align;   /* for alignment to long boundary */
+
+union header {        /* block header (free or used) */
   struct {
-    union header *ptr;
-    uint size;
+    union header *ptr;  /* next block if on free list */
+    uint size;          /* size of this block */
   } s;
-  Align x;
+  Align x;              /* force alignment of blocks (never used) */
 };
 
 typedef union header Header;
 
-static Header base;
-static Header *freep;
+static Header base;   /* 空闲块链表的头部 */
+static Header *freep; /* 上一次找到空闲块的地方 */
 
 void
 free(void *ap)
@@ -67,21 +78,23 @@ malloc(uint nbytes)
   uint nunits;
 
   nunits = (nbytes + sizeof(Header) - 1)/sizeof(Header) + 1;
-  if((prevp = freep) == 0){
+  if((prevp = freep) == 0){     /* no free list yet */
     base.s.ptr = freep = prevp = &base;
     base.s.size = 0;
   }
+  /* 找符合条件的空闲块 */
   for(p = prevp->s.ptr; ; prevp = p, p = p->s.ptr){
-    if(p->s.size >= nunits){
-      if(p->s.size == nunits)
+    if(p->s.size >= nunits){    /* 找到足够大的空闲块 */
+      if(p->s.size == nunits)   /* 大小正好 */
         prevp->s.ptr = p->s.ptr;
-      else {
+      else {                    /* If malloc() will use a part of a free segement, this is split into two parts. */
+                                /* The first remains free, the second will be the one returned */
         p->s.size -= nunits;
         p += p->s.size;
         p->s.size = nunits;
       }
       freep = prevp;
-      return (void*)(p + 1);
+      return (void*)(p + 1);    /* 1是头部的大小 */
     }
     if(p == freep)
       if((p = morecore(nunits)) == 0)
